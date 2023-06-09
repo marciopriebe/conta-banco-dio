e97e10573e30159470c25d87bfea2d32a95e2a83
marciopriebe <m-priebe@hotmail.com>
2023-06-09 06:41:26 -0300

    Criado contaBanco e ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ContaTErminal {
+public class ContaTerminal {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
